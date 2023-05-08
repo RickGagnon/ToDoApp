bc76b810cb8f47c84e5921072e5797ea720f33db
RickGagnon <AtessRickGagnon@Outlook.com>
2023-05-08 11:40:47 -0400

    missing

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Item } from './item';
-import { HttpClient } from '@angular/common/http';
+import { Category } from './category';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UserItem } from './userItem';
+import { environment} from "../environments/environment"
+
+
+
+  const httpOptions = {
+    headers:new HttpHeaders({
+      'Access-COntrol-Allow-Origin':'*'
+     })
+  }
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor(private http:HttpClient) { }
+
+
+
+  constructor(private http:HttpClient) { 
+
+    
+  }
+
+getUserItems():Observable<UserItem[]>{
+  //return this.http.get<UserItem[]>("https://atesstodoapi.azurewebsites.net/api/item/useritems")
+  return this.http.get<UserItem[]>(environment.apiUrl + "item",httpOptions);
+}
+
 
   getTodoItems():Observable<Item[]>{
-    return this.http.get<Item[]>("https://atesstodoapi.azurewebsites.net/api/item")
+    return this.http.get<Item[]>(environment.apiUrl + "item",httpOptions);//"https://atesstodoapi.azurewebsites.net/api/item")
 /*     let todoItems:Array<TodoItem>=[
     {"itemDescription":"Pick up Milk","itemCompleted":false},
     {"itemDescription":"Dr Appointment at 3:00pm","itemCompleted":false},
@@ -20,15 +46,18 @@ export class TodoService {
     //return todoItems
   }
  deleteTodoItem(index:number):Observable<Item[]>{
-  return this.http.delete<Item[]>("https://atesstodoapi.azurewebsites.net/api/item/"+index);
+  return this.http.delete<Item[]>(environment.apiUrl + "item"+index,httpOptions);
   
  }
  addTodoItem(todoItem:Item){
-  return this.http.post<Item>("https://atesstodoapi.azurewebsites.net/api/item",todoItem);
+  return this.http.post<Item>(environment.apiUrl + "item",todoItem,httpOptions);
 
 }
+
+
+
 updateTodoItem(todoItem:Item):Observable<Item>{
-  return this.http.put<Item>("https://atesstodoapi.azurewebsites.net/api/item/"+todoItem.itemId,todoItem);
+  return this.http.put<Item>(environment.apiUrl + "item"+todoItem.itemId,todoItem,httpOptions);
 
 }
 }
